074f12bc6719b29a988e8ace09bbb908e7f255a4
Rones Júnior <rones@rocketmail.com>
2026-02-25 08:35:35 -0300

    commit correção fetch

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -182,8 +182,8 @@ class Api {
 
 /// Cria e exporta uma constante 'api' que contém uma instância da classe 'Api' acima para buscar dados de uma API
 export const api = new Api({
-  // baseUrl: "https://backend-2xrp.onrender.com",
-  baseUrl: "http://localhost:5001",
+  baseUrl: "https://backend-2xrp.onrender.com",
+  // baseUrl: "http://localhost:5001",
   headers: {
     "Content-Type": "application/json",
   },
